5a619f43c54116ab0da3d51925f7ab072c293e1c
michellejanosi <michelle.janosi@gmail.com>
2023-07-13 11:05:18 -0400

    Add assignment solution

--- fundamentals/oop/bank_account.py
+++ fundamentals/oop/bank_account.py
@@ -37,11 +37,78 @@ class BankAccount:
         for account in cls.all_accounts:
             account.display_account_info()
 
-account1 = BankAccount()
-account2 = BankAccount()
 
-account1.deposit(1250).deposit(2278).deposit(525).withdraw(1500).yield_interest().display_account_info()
+class User:
+    all_users = []
 
-account2.deposit(9082).deposit(1034).withdraw(2376).withdraw(1346).withdraw(145).withdraw(50).yield_interest().display_account_info()
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.accounts = {}  # Dictionary to store the different types of bank accounts
+        User.all_users.append(self)
 
-BankAccount.display_all_accounts_info()
+    def add_account(self, type_of_account, int_rate, initial_balance = 0):
+        # Create a new bank account and add it to the accounts dictionary
+        self.accounts[type_of_account] = BankAccount(int_rate, initial_balance)
+        return self
+
+    def make_deposit(self, type_of_account, amount):
+        if type_of_account in self.accounts:
+            self.accounts[type_of_account].deposit(amount)
+        else:
+            print(f"No {type_of_account} exists for {self.name}.")
+        return self
+
+    def make_withdrawal(self, type_of_account, amount):
+        if type_of_account in self.accounts:
+            self.accounts[type_of_account].withdraw(amount)
+        else:
+            print(f"No {type_of_account} exists for {self.name}.")
+        return self
+
+    def display_user_balance(self, type_of_account):
+        if type_of_account in self.accounts:
+            self.accounts[type_of_account].display_account_info()
+        else:
+            print(f"No {type_of_account} exists for {self.name}.")
+        return self
+
+
+    def transfer_money(self, type_of_account, amount, other_user):
+        if type_of_account in self.accounts and type_of_account in other_user.accounts:
+            if self.accounts[type_of_account].withdraw(amount):
+                other_user.accounts[type_of_account].deposit(amount)
+                print(f"Transferred ${amount} from {self.name}'s {type_of_account} to {other_user.name}'s {type_of_account}")
+            else:
+                print(f"Insufficient funds in {self.name}'s {type_of_account}")
+        else:
+            print(f"No {type_of_account} account exists for either {self.name} or {other_user.name}")
+        return self
+
+    @classmethod
+    def display_all_accounts_info(cls):
+        for user in cls.all_users:
+            print(f"User: {user.name}, Email: {user.email}")
+            for account_type, account in user.accounts.items():
+                print(f"Account Type: {account_type}")
+                account.display_account_info()
+
+
+user1 = User("Alice", "alice@gmail.com")
+user1.add_account("Checking", 0.01, 500)
+user1.add_account("Savings", 0.02, 1000)
+
+user2 = User("Bob", "bob@gmail.com")
+user2.add_account("Checking", 0.01, 700)
+user2.add_account("Savings", 0.02, 1500)
+
+user1.make_deposit("Checking", 500).make_withdrawal("Savings", 200).display_user_balance("Checking").display_user_balance("Savings")
+
+user2.make_deposit("Saving", 500).make_withdrawal("Checking", 200).display_user_balance("Checking").display_user_balance("Savings")
+
+# Transferring money from Alice's checking account to Bob's saving account
+user1.transfer_money("Checking", 200, user2)
+user1.display_user_balance("Checking")
+user2.display_user_balance("Savings")
+
+User.display_all_accounts_info()
