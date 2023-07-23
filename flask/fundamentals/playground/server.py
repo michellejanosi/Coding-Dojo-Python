e9d16f7ac3b86e1c11631bd4a9e037a14fb5b47b
michellejanosi <michelle.janosi@gmail.com>
2023-07-23 15:01:17 -0400

    Add error handling

--- flask/fundamentals/playground/server.py
+++ flask/fundamentals/playground/server.py
@@ -4,15 +4,19 @@ app = Flask(__name__)
 
 @app.route('/play')
 def play():
-    return render_template("index.html", times=3, color='blue')
+    return render_template("index.html", times=3, color='skyblue')
 
 @app.route('/play/<int:x>')
 def play_x(x):
-    return render_template("index.html", times=x, color='blue')
+    return render_template("index.html", times=x, color='skyblue')
 
 @app.route('/play/<int:x>/<string:color>')
 def play_x_color(x, color):
     return render_template("index.html", times=x, color=color)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "Sorry! No response. Try again.", 404
 
 if __name__=="__main__":
     app.run(debug=True)
