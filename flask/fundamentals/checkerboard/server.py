8894ee58af20b2b82b09c48fc573d8ce519b6a94
michellejanosi <michelle.janosi@gmail.com>
2023-07-24 11:20:08 -0400

    Add elements and additional route for colors

--- flask/fundamentals/checkerboard/server.py
+++ flask/fundamentals/checkerboard/server.py
@@ -4,15 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def checkerboard():
-    return render_template('index.html', x=8, y=8)
+    return render_template('index.html', x=8, y=8, color1='black', color2='red')
 
 @app.route('/<int:x>')
 def checkerboard_x(x):
-    return render_template('index.html', x=x, y=8)
+    return render_template('index.html', x=x, y=8, color1='black', color2='red')
 
 @app.route('/<int:x>/<int:y>')
 def checkerboard_xy(x, y):
-    return render_template('index.html', x=x, y=y)
+    return render_template('index.html', x=x, y=y, color1='black', color2='red')
+
+@app.route('/<int:x>/<int:y>/<color1>/<color2>')
+def checkerboard_xy_color(x, y, color1, color2):
+    return render_template('index.html', x=x, y=y, color1=color1, color2=color2)
 
 if __name__=="__main__":
     app.run(debug=True)
